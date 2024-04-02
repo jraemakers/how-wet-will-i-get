cdee49f14bd60f70e91a16f2c61e098aeb0cfff1
Justin509805 <509805@student.fontys.nl>
2024-04-02 09:55:23 +0200

    added title, bg color, line

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -5,16 +5,40 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Color(0xFF0077B6),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 68.0),
+              child: Center(
+                child: Text(
+                  'HOW WET WILL I GET?',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 400),
+            Divider(
+              color: Colors.white,
+              thickness: 1.0,
+            ),
+            // Add more widgets here if needed
+          ],
         ),
       ),
     );
   }
 }
+
