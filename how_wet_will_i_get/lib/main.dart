0263580ba75c6a9d64858dc8e80fdc5810dff2d2
Joost Raemakers <jraemakers@outlook.com>
2024-04-10 11:59:12 +0200

    Update assets/images paths in pubspec.yaml

--- how_wet_will_i_get/lib/main.dart
+++ how_wet_will_i_get/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   _MainAppState createState() => _MainAppState();
@@ -16,76 +16,104 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   Map<String, dynamic>? _weatherData;
-  Map<String, dynamic>? _forecastData;
   bool _isLoading = true;
+  String _currentLocation = '';
   String _weatherImage = '';
-  String _forecastImage = '';
   String _weatherDescription = '';
+
+  late TextEditingController _locationController;
 
   @override
   void initState() {
     super.initState();
     _getLocation();
+    _locationController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _locationController.dispose();
+    super.dispose();
   }
 
   Future<void> _getLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.best);
-      _fetchData(position.latitude, position.longitude);
+      _fetchWeather(position.latitude, position.longitude);
     } catch (e) {
       print(e);
     }
   }
 
-  Future<void> _fetchData(double latitude, double longitude) async {
+  Future<void> _fetchWeather(double latitude, double longitude) async {
     const apiKey = 'e703b4fa9b3202d8da64ac0141c7a225';
     final weatherUrl =
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
-
-    final forecastUrl =
-        'https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric&cnt=2';
+        'https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric&cnt=1';
 
     try {
       final currentWeatherResponse = await http.get(Uri.parse(weatherUrl));
-      final forecastResponse = await http.get(Uri.parse(forecastUrl));
 
-      if (currentWeatherResponse.statusCode == 200 &&
-          forecastResponse.statusCode == 200) {
+      if (currentWeatherResponse.statusCode == 200) {
         final currentWeatherData = jsonDecode(currentWeatherResponse.body);
-        final forecastData = jsonDecode(forecastResponse.body);
 
         setState(() {
           _weatherData = currentWeatherData;
-          _forecastData = forecastData;
+          _currentLocation = _weatherData!["city"]["name"];
           _isLoading = false;
         });
 
-        if (_weatherData!["rain"] == null) {
+        if (_weatherData!["list"][0]["rain"] == null) {
           _weatherImage = 'rain-none.png';
           _weatherDescription = 'check.png';
-        } else if (_weatherData!["rain"]["1h"] < 2.5) {
+        } else if (_weatherData!["list"][0]["rain"]["3h"] < 2.5) {
           _weatherImage = 'rain-licht.png';
           _weatherDescription = 'hoodie.png';
-        } else if (_weatherData!["rain"]["1h"] < 7.6) {
+        } else if (_weatherData!["list"][0]["rain"]["3h"] < 7.6) {
           _weatherImage = 'rain-normal.png';
           _weatherDescription = 'protection.png';
         } else {
           _weatherImage = 'rain-hard.png';
           _weatherDescription = 'raincoat.png';
         }
-
-        if (_forecastData!["list"][1]["rain"] == null) {
-          _forecastImage = 'rain-none.png';
-        } else if (_forecastData!["list"][1]["rain"]["3h"] < 2.5) {
-          _forecastImage = 'rain-licht.png';
-        } else if (_forecastData!["list"][1]["rain"]["3h"] < 7.6) {
-          _forecastImage = 'rain-normal.png';
-        } else {
-          _forecastImage = 'rain-hard.png';
-        }
       } else {
         throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> _handleSubmit(String location) async {
+    const apiKey = 'e703b4fa9b3202d8da64ac0141c7a225';
+    final forecastUrl =
+        'https://api.openweathermap.org/data/2.5/forecast?q=$location&appid=$apiKey&units=metric&cnt=2';
+
+    try {
+      final currentForecastResponse = await http.get(Uri.parse(forecastUrl));
+
+      if (currentForecastResponse.statusCode == 200) {
+        final currentForecastData = jsonDecode(currentForecastResponse.body);
+
+        setState(() {
+          _weatherData = currentForecastData;
+        });
+
+        if (_weatherData!["list"][1]["rain"] == null) {
+          _weatherImage = 'rain-none.png';
+          _weatherDescription = 'check.png';
+        } else if (_weatherData!["list"][1]["rain"]["3h"] < 2.5) {
+          _weatherImage = 'rain-licht.png';
+          _weatherDescription = 'hoodie.png';
+        } else if (_weatherData!["list"][1]["rain"]["3h"] < 7.6) {
+          _weatherImage = 'rain-normal.png';
+          _weatherDescription = 'protection.png';
+        } else {
+          _weatherImage = 'rain-hard.png';
+          _weatherDescription = 'raincoat.png';
+        }
+      } else {
+        throw Exception('Failed to load forecast data');
       }
     } catch (e) {
       print('Error: $e');
@@ -108,52 +136,16 @@ class _MainAppState extends State<MainApp> {
                   ),
                 ),
               )
-            : Stack(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(top: 68.0),
-                        child: Center(
-                          child: Text(
-                            'HOW WET WILL I GET?',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w800,
-                              fontFamily: 'Poppins',
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 38.0),
-                        child: Center(
-                          child: Image.asset(
-                            'assets/images/$_weatherImage',
-                            height: 180,
-                            width: 180,
-                          ),
-                        ),
-                      ),
-                      Center(
-                        child: Image.asset(
-                          'assets/images/$_weatherDescription',
-                          height: 180,
-                          width: 150,
-                        ),
-                      ),
-                      const SizedBox(height: 1),
-                      const Divider(
-                        color: Colors.white,
-                        thickness: 1.0,
-                      ),
-                      const Center(
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 68.0),
+                      child: Center(
                         child: Text(
-                          '+3HR PREDICTION',
+                          'HOW WET WILL I GET?',
                           style: TextStyle(
                             fontSize: 24,
                             color: Colors.white,
@@ -162,36 +154,100 @@ class _MainAppState extends State<MainApp> {
                           ),
                         ),
                       ),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 80.0, top: 15),
-                            child: Text(
-                              'Temp',
-                              style: TextStyle(
+                    ),
+                    const SizedBox(height: 16),
+                    Positioned(
+                      bottom: 10,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.location_on,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(width: 16),
+                            Text(
+                              _currentLocation.toUpperCase(),
+                              style: const TextStyle(
                                 fontSize: 16,
                                 color: Colors.white,
-                                fontWeight: FontWeight.w300,
+                                fontWeight: FontWeight.bold,
                                 fontFamily: 'Poppins',
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                    ),
+                    const SizedBox(height: 16),
+                    const Icon(
+                      Icons.arrow_downward,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                      child: TextField(
+                        controller: _locationController,
+                        onSubmitted: _handleSubmit,
+                        decoration: const InputDecoration(
+                          hintText: 'WHERE TO?',
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.location_on,
+                            color: Colors.white,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 32.0),
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32.0),
+                      child: Center(
+                        child: Image.asset(
+                          'assets/images/$_weatherImage',
+                          height: 180,
+                          width: 180,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32.0),
+                      child: Center(
+                        child: Image.asset(
+                          'assets/images/$_weatherDescription',
+                          height: 180,
+                          width: 150,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 100.0),
-                            child: Text(
-                              '${_forecastData!["list"][1]["main"]["temp"].round()}',
-                              style: const TextStyle(
-                                fontSize: 66,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Poppins',
-                              ),
+                          Text(
+                            '${_weatherData!["list"][0]["main"]["temp"].round()}',
+                            style: const TextStyle(
+                              fontSize: 66,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Poppins',
                             ),
                           ),
                           const Text(
@@ -203,48 +259,11 @@ class _MainAppState extends State<MainApp> {
                               fontFamily: 'Poppins',
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10.0),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/images/$_forecastImage',
-                                height: 100,
-                                width: 100,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    left: 0,
-                    right: 0,
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/placeholder.png',
-                            height: 16,
-                            width: 16,
-                          ),
-                          const SizedBox(width: 5),
-                            Text(
-                            '${_forecastData!["city"]["name"]}'.toUpperCase(),
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Poppins',
-                            ),
-                          ),
                         ],
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
       ),
     );
